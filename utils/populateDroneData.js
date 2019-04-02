20cf83baf3b37a738134f25e5af67d3408691a55
Andrew <arhorn@smcm.edu>
2019-04-01 19:17:30 -0500

    Added populateDroneData.js

---
+++ utils/populateDroneData.js
@@ -0,0 +1,33 @@
+const moment = require('moment');
+const { getLatLongAroundHouston } = require('./getLatLongAroundHouston');
+const { getUom } = require('./getUom');
+const { getAccuracy } = require('./getAccuracy');
+
+const populateDroneData = () => {
+  const data = []
+  let metric = 275;
+  let secondsLeft = 1800;
+  let timestamp = moment().unix();
+  let uom = getUom();
+  let accuracy;
+  while(secondsLeft > 0) {
+    const { latitude, longitude } = getLatLongAroundHouston(timestamp % 3600);
+    metric = incrementMetric(metric);
+    accuracy = getAccuracy();
+    data.push({
+      timestamp,
+      metric,
+      latitude,
+      longitude,
+      uom,
+      accuracy
+    })
+    timestamp -= 4;
+    secondsLeft -= 4;
+  }
+  return data;
+}
+
+const incrementMetric = currentMetric => currentMetric + (Math.random() * 2 - 1);
+
+module.exports = { populateDroneData };
