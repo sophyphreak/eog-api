b0b2c752ecd1c1e149607a9cfb0dc9baa862cca3
Andrew <arhorn@smcm.edu>
2019-04-01 20:06:34 -0500

    filled out incrementDroneData.js

--- utils/incrementDroneData.js
+++ utils/incrementDroneData.js
@@ -1,7 +1,25 @@
-const incrementDroneData = (droneData, metric) => {
+const moment = require('moment');
+const { incrementMetric } = require('./incrementMetric');
+const { getUom } = require('getUom');
+const { getAccuracy } = require('./getAccuracy');
+
+const incrementDroneData = droneData => {
   const { data } = droneData;
-  // do stuff
-  // return { data }
+  const timestamp = moment().unix();
+  const metric = incrementMetric(data[0].metric);
+  const { latitude, longitude } = getLatLongAroundHouston(timestamp % 3600);
+  const uom = getUom();
+  const accuracy = getAccuracy();
+  data.unshift({
+    timestamp,
+    metric,
+    latitude,
+    longitude,
+    uom,
+    accuracy
+  });
+  data.pop();
+  return { data };
 };
 
 module.exports = { incrementDroneData };
